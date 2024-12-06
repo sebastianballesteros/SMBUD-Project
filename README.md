2dfdfdb8e743bfeafb7648c03977b9d2d2c81ee4
abrilcano <147972578+abrilcano@users.noreply.github.com>
2024-12-06 15:12:40 +0100

    Added Mongo script and data

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SMBUD-Project
-SMBUD Project 
+
+SMBUD Project
+
+Connection to MongoDB Cluster 
+
+With VS Code Extension
+
+```
+mongodb+srv://<db_username>:<db_password>@smbud-project.cazsb.mongodb.net/
+```
+
+With Shell
+
+```
+mongosh "mongodb+srv://smbud-project.cazsb.mongodb.net/" --apiVersion 1 --username <db_username>
+```
---
+++ mongodb/preprocessing.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import json
+
+# Load the CSV file
+csv_file = 'survey_results_public.csv'
+df = pd.read_csv(csv_file)
+
+#Drop un necessary columns
+
+# Drop the 'check' column
+if 'check' in df.columns:
+    df.drop('check', axis=1, inplace=True)
+
+
+# Drop columns with more than 80% missing values
+threshold = 0.60  
+df = df.loc[:, df.isna().mean() <= threshold]
+
+# Convert columns with multiple answers into arrays
+for col in df.select_dtypes(include='object').columns:
+    if df[col].str.contains(';', na=False).any():
+        df[col] = df[col].apply(lambda x: x.split(';') if pd.notna(x) else [])
+
+# Convert to JSON
+json_file = 'survey_results.json'
+df.to_json(json_file, orient='records', lines=True, indent=4)
+
+print(f"CSV converted to JSON and saved as {json_file}")
+
