6320c88e46f14bc8aa4b5475332088daa8461b48
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2024-12-09 14:57:54 +0100

    Fixed url

--- mongodb/client/src/config.js
+++ mongodb/client/src/config.js
@@ -1,5 +1,5 @@
 const url = (process.env?.NODE_ENV === 'development')
                                   ? 'http://localhost:8080/'
-                                  : 'https://smbud-project-19y6.onrender.com'
+                                  : 'https://smbud-project-19y6.onrender.com/'
 
 export default url
