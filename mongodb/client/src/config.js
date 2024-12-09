24b33be2665e4215b5b1bd32e519621546c88704
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2024-12-09 14:54:17 +0100

    Modified config to fetch from the hosted application in production

--- mongodb/client/src/config.js
+++ mongodb/client/src/config.js
@@ -1,3 +1,5 @@
-const url = 'http://localhost:8080/'
+const url = (process.env?.NODE_ENV === 'development')
+                                  ? 'http://localhost:8080/'
+                                  : 'https://smbud-project-19y6.onrender.com'
 
 export default url
