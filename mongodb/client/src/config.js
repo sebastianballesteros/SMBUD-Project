18ace82925df7caf336774624688d0c785aecd36
YoSoyElLobo <a20170569@pucp.edu.pe>
2024-12-12 21:46:21 +0100

    Update config.js
    React uses env variables that start with REACT_APP

--- mongodb/client/src/config.js
+++ mongodb/client/src/config.js
@@ -1,5 +1,4 @@
-const url = (process.env?.NODE_ENV === 'development')
+const url = (process.env?.REACT_APP_NODE_ENV === 'development')
                                   ? 'http://localhost:8080/'
                                   : 'https://smbud-project-19y6.onrender.com/'
-
 export default url
